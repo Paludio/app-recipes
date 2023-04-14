586cf5d1646a8171066b1e998c465efaf07f635d
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2023-04-14 11:39:21 -0300

    :art: build: installed the lint to the css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import '../styles/login/login.sass'
+import '../styles/login/login.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
